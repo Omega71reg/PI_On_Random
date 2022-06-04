598a495e75d24716d7530d4e913f3a084d573d27
Omega71reg <Omega71reg@Mail.ru>
2022-06-04 19:50:35 +0300

    Добавьте файлы проекта.

---
+++ PI_on_Random/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+namespace PI_on_Random
+{
+    class Program
+    {
+        private static readonly Random Rand = new Random();
+        static void Main(string[] args)
+        {
+            int countInCircle = 0;
+            int countAll = 0;
+            Console.WriteLine("Введи точность вычесления: ");
+            countAll = Int32.Parse(Console.ReadLine()?? throw new InvalidOperationException());
+
+            for (int i = 0; i < countAll; i++)
+            {
+                double x = RandFrom0to1();
+                double y = RandFrom0to1();
+
+                if (Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)) <= 1)
+                countInCircle++;
+            }
+
+            double pi = 4 * (double)countInCircle / countAll;
+
+            Console.WriteLine("PI = " + pi);
+        }
+        private static double RandFrom0to1()
+        {
+            return Rand.NextDouble();
+        }
+    }
+}
